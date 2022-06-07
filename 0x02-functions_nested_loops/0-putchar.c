92baf45fd74ef73bede94aba235364acb36a9ead
larry-techs <gichukimutethia001@gmail.com>
2022-06-07 04:45:29 -0700

    first nested functions commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 success
+*/
+
+int main(void)
+{
+puts("_putchar\n");
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+c functionss and loops
